a0819d674fb965d58f7e98bf1a9ea4277f8c5f93
ratkosrb <ratkomladic2@abv.bg>
2021-08-24 18:48:04 +0300

    Rename and implement all aura interrupt flags.

--- src/game/PlayerBots/PartyBotAI.cpp
+++ src/game/PlayerBots/PartyBotAI.cpp
@@ -453,7 +453,7 @@ bool PartyBotAI::ShouldAutoRevive() const
 
 bool PartyBotAI::CanUseCrowdControl(SpellEntry const* pSpellEntry, Unit* pTarget) const
 {
-    if ((pSpellEntry->AuraInterruptFlags & AURA_INTERRUPT_FLAG_DAMAGE) &&
+    if (pSpellEntry->HasAuraInterruptFlag(AURA_INTERRUPT_DAMAGE_CANCELS) &&
         AreOthersOnSameTarget(pTarget->GetObjectGuid()))
         return false;
 
